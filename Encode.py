6bd751c12ed611930e89fb534ca7ee91b6500adb
Rajasree Baruri <rbaruri2002@gmail.com>
2023-08-09 16:47:05 +0530

    Updated

--- Encode.py
+++ Encode.py
@@ -1,16 +1,16 @@
 def EncodeQR(name):
+    pad = 0b10000000
     if len(name) < 1 or len(name) > 5:
         print("Product name should be 1 to 5 characters long.")
     else:
         encoded_qr = []
-        
         for char in name:
             binary_value = format(ord(char), '08b')
-            encoded_qr.append(binary_value[1:])
+            shift = pad << 1
+            encoded_qr.append(str(bin(shift)[2:]) + binary_value)
             
-        return encoded_qr
+        return ''.join(encoded_qr)
 
 product_name = input("Enter Product Name: ")
 encoded_result = EncodeQR(product_name)
 print(encoded_result)
-#padding left, list to string encoded_qr
